a10c1116386b1fb78949fbd7736b9b4a1ae5f790
Nils <nils.schlatter@yetnet.ch>
2024-05-25 19:20:26 +0200

    implemented model
    added elbow to determine k (failed)
    roc auc and metrics implemented

--- KNNv1.2.py
+++ KNNv1.2.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from imblearn.over_sampling import SMOTE
@@ -7,6 +8,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import precision_recall_fscore_support as  score
 from sklearn import metrics
 
 def evaluate_features_knn(k, n_neighbors=5):
@@ -34,7 +36,23 @@ y = data.iloc[:, -1].values
 smote = SMOTE(random_state=42)
 X_resampled, y_resampled = smote.fit_resample(X, y)
 
-num_features = [10, 20, 30, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 100]
+
+x_training_data, x_test_data, y_training_data, y_test_data = train_test_split(X, y, test_size = 0.3)
+
+error_rates = []
+
+for i in np.arange(1, 101):
+    new_model = KNeighborsClassifier(n_neighbors = i)
+    new_model.fit(x_training_data, y_training_data)
+    new_predictions = new_model.predict(x_test_data)
+    print(np.mean(new_predictions != y_test_data))
+    error_rates.append(np.mean(new_predictions != y_test_data))
+
+plt.figure(figsize=(16,12))
+plt.plot(error_rates)
+plt.show()
+
+num_features = [10,15,20,25,30,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,100]
 
 results = {k: evaluate_features_knn(k) for k in num_features}
 
@@ -50,3 +68,53 @@ plt.title('Feature Selection using ANOVA for KNN')
 plt.grid()
 plt.savefig('../output/feature_selection_knn_smaller.png')
 plt.show()
+
+best_k = 70
+selector = SelectKBest(f_classif, k=best_k)
+X_resampled_new = selector.fit_transform(X_resampled, y_resampled)
+X_new = selector.transform(X)
+
+feature_scores = selector.scores_
+feature_score_dict = dict(zip(data.columns[:-1], feature_scores))
+# Sort the dictionary by scores in descending order
+sorted_feature_score_dict = dict(sorted(feature_score_dict.items(), key=lambda item: item[1], reverse=True))
+# Print the top features and their scores
+top_features = list(sorted_feature_score_dict.keys())[:39]  # Print the top 10 features
+print("Top features and their scores:")
+for feature in top_features:
+    print(f"Feature: {feature}, Score: {sorted_feature_score_dict[feature]}")
+
+
+# Standardize the features
+scaler = StandardScaler()
+X_resampled_new = scaler.fit_transform(X_resampled_new)
+X_new = scaler.transform(X_new)
+
+
+split = 0.3
+
+X_train, X_test, y_train, y_test = train_test_split(X_new, y, test_size=split, random_state=42)
+
+knn = KNeighborsClassifier(n_neighbors=597)
+knn.fit(X_train, y_train)
+fpr, tpr, thresholds = metrics.roc_curve(y_test, knn.predict_proba(X_test)[:,1])
+auc = metrics.roc_auc_score(y_test,knn.predict(X_test))
+plt.plot(fpr, tpr, label='%s ROC (area = %0.2f)' % (1, auc))
+yhat = knn.predict(X_test)
+
+precision, recall, fscore, support = score(y_test, yhat)
+
+plt.plot([0, 1], [0, 1],'r--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel("False Positive Rate")
+plt.ylabel("True Positive Rate")
+plt.title("ROC-Curve")
+plt.legend(loc="lower right")
+plt.show()
+plt.savefig(f"../output/ROC_{str(split)}_split.png")
+
+print('precision: {}'.format(precision))
+print('recall: {}'.format(recall))
+print('fscore: {}'.format(fscore))
+print('support: {}'.format(support))
